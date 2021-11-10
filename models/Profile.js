24bd78582eed890797c1216a8761d319b17624c3
lexreal25 <40786380+lexreal25@users.noreply.github.com>
2021-11-10 16:35:31 +0000

    modified profile

--- models/Profile.js
+++ models/Profile.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const ProfileSchema = new mongoose.Schema({
     name:{ type: String, required:true},
-    locaton:{type: String, required:true},
+    location:{type: String, required:true},
     age: {type: Number, required: true},
     contact: {type: Number, required:true, unique:true},
     gender: ["Male", "Female"],
